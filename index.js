a52c7443631eb4468c834a32867f238513345f43
Svetlana <lineup@example.com>
2020-08-07 16:37:30 +0300

    cherrypik max 6

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ document.addEventListener('DOMContentLoaded', function(){
     console.log('commit 1')
     console.log('commit 2')
     console.log('commit 3')
+    console.log('commit 4')
+    console.log('commit 5')
+    console.log('commit 6')
 
   });
