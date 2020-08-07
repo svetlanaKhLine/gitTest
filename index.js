47f1c8bbb9922356918e67d3977ae22a966a1560
Svetlana <lineup@example.com>
2020-08-07 16:28:04 +0300

    2

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){ 
     console.log('downloaded');
-    console.log('secondCommit')
+    console.log('secondCommit');
+    console.log('commit 1 - square')
+    console.log('commit 2')
+
   });
